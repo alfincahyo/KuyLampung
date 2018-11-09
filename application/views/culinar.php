dbc312ca866e8f7bfaed08a4d306806faef33011
Frisky Riana Kesumadewi <38089317+friskyriana@users.noreply.github.com>
2018-11-09 22:13:27 +0700

    Add files via upload

--- application/views/culinar.php
+++ application/views/culinar.php
@@ -5,29 +5,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Culinary</title>
-	<meta charset="utf-8">
+  <title>Culinary</title>
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
-	
+  
 </head>
 
 <body>
- 	<div class="container">
+  <div class="container">
 
     <h1 style="color: black; border-color: black; margin-left: -20px; margin-top: 20px;">CULINARY</h1>
 
-	<hr class="featurette-divider">
+  <hr class="featurette-divider">
 
         <div class="row featurette">
           <div class="col-md-7" style="margin-top: 10px;">
             <h2 class="featurette-heading">SERUIT</h2>
             <p class="lead">Seruit merupakan salah satu makanan khas Lampung yang sering dihidangkan saat ada acara keluarga, pernikahan, acara adat serta acara keagamaan. Sementara itu, seruit ini juga sering dijadikan makanan pokok yang dikonsumsi sehari-hari oleh masyarakat Pepadun. Seruit merupakan sebuah makanan yang terbuat dari ikan bakar yang mix dengan berbagai sambal terasi khas kota Bandar Lampung seperti tempoyak ataupun ada pula yang menambahkan mangga.Pada dasarnya ikan-ikan yang digunakan untuk membuat seruit cukuplah beragam, namun ikan yang paling sering digunakan ialah ikan yang berasal dari sungai seperti ikan baung, ikan balide, ikan layis dan lainnya. Saat menyantap seruit ini biasanya akan ditambahkan lalapan agar memberikan cita rasa yang lebih lezat dan nikmat.
-			</p>
+      </p>
+      <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          9.5 / 10
           </div>
 
           <div class="col-md-5">
@@ -43,6 +52,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <h2 class="featurette-heading">TEMPOYAK</h2>
             <p class="lead">Tempoyak merupakan sebuah makanan yang menggunakan bahan dasar buah durian yang sudah difermentasikan dan segera mengolahnya dengan campuran ikan. Ikan yang biasanya digunakan cukup beragam seperti ikan mujair, ikan teri, ikan mas dan lainnya. <br>
             Bagi anda yang ingin membuat sambal maka harus mencampurkan bawang merah, bawang putih, cabe yang telah dihaluskan ataupun dipotong-potong kecil, kemudian tambahkan sedikit gula. Kemudian anda bisa mengorengnya hingga 3 menit, selanjutnya tempoyak bisa langsung dihidangkan dan dimakan.</p>
+             <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/balam/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/balam/none.png"  height="50px" width="50px";> 
+          9.5 / 10
           </div>
           <div class="col-md-5 order-md-1">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/tempoyak.jpg" data-holder-rendered="true">
@@ -68,7 +86,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7 order-md-2 " style="margin-top: 10px;">
             <h2 class="featurette-heading">ENGKAK</h2>
             <p class="lead">Engkak merupakan makanan yang memiliki rasa yang cukup manis. Bahan utama untuk pembuatan engkak ialah telur dan mentega yang dihaluskan menjadi tekstur yang sangat lembek. Engkak ini sangat cocok disantap sebagai makanan ringan dan juga sarapan. Bahan-bahan yang digunakan untuk pembuatan engkak ialah gula pasir, tepung ketan putih, telur ayam, susu kental manis, santan serta mentega.
-			Selanjutnya semua bahan tesebut bisa anda campurkan menjadi satu lalu aduk hingga merata. Setelahnya anda bisa memasukan adonan tersebut ke dalam loyang yang sudah dioleskan mentega dan juga di beri alas kertas berih. Anda bisa memanggang kue ini layaknya lapis segit.</p>
+      Selanjutnya semua bahan tesebut bisa anda campurkan menjadi satu lalu aduk hingga merata. Setelahnya anda bisa memasukan adonan tersebut ke dalam loyang yang sudah dioleskan mentega dan juga di beri alas kertas berih. Anda bisa memanggang kue ini layaknya lapis segit.</p>
           </div>
           <div class="col-md-5 order-md-1">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/engkak2.jpg" data-holder-rendered="true">
@@ -93,7 +111,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7 order-md-2" style="margin-top: 10px;">
             <h2 class="featurette-heading">PANGGANG LAMPUNG</h2>
             <p class="lead">Panggang merupakan makanan khas dari Lampung yang proses pembuatannya harus diasapi ataupun dipanggang. Sehingga tak heran jika makanan khas Lampung ini dikenal dengan nama panggang.
-			Panggang menggunakan ikan sebagai bahan utama dalam pembuatannya yang kemudian di panggang ataupun di asapi di atas bara api. Proses pemanganan ini juga tidak membutuhkan waktu yang lama, hanya saja anda harus menunggu hingga ikannya matang. Apabila ikan tersebut sudah matang maka anda bisa langsung memakannya.</p>
+      Panggang menggunakan ikan sebagai bahan utama dalam pembuatannya yang kemudian di panggang ataupun di asapi di atas bara api. Proses pemanganan ini juga tidak membutuhkan waktu yang lama, hanya saja anda harus menunggu hingga ikannya matang. Apabila ikan tersebut sudah matang maka anda bisa langsung memakannya.</p>
           </div>
           <div class="col-md-5 order-md-1">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/ikanpanggang.png" data-holder-rendered="true">
@@ -130,7 +148,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7" style="margin-top: 10px;">
             <h2 class="featurette-heading">SAMBAL ASAM KEMBANG </h2>
             <p class="lead"> Sambal asam kembang merupakan salah satu makanan khas Lampung yang memiliki rasa cukup lezat dan nikmat. Jika mendengar namanya pasti anda mengira sambal ini terbuat dari bahan utamanya asam. Namun perlu diketahui, jika dalam proses pembuatan sambal asam kembang ini sama sekali tidak menggunakan asam, akan tetapi terbuat dari terasi yang dicampur dengan buah kemang seperti buah mangga muda.
-			Sambal asam kembang ini juga bisa disantap dengan makanan khas dari kota Lampung lainnya. Selain itu, anda juga bisa menjadikan sambal asam kembang ini sebagai oleh-oleh dari kota Lampung. </p>
+      Sambal asam kembang ini juga bisa disantap dengan makanan khas dari kota Lampung lainnya. Selain itu, anda juga bisa menjadikan sambal asam kembang ini sebagai oleh-oleh dari kota Lampung. </p>
           </div>
           <div class="col-md-5">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 380px;" src="<?=base_url()?>assets/img/culinary/sambalasamkembang.jpg" data-holder-rendered="true">
