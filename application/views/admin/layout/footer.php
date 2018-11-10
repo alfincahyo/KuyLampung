fce26fcd08c857eb3515e4a98b630a50aa570249
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-10 07:38:20 +0700

    Create footer.php

---
+++ application/views/admin/layout/footer.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/header.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+	<footer class="container" style="margin-top: 20px; border: 2px;">
+		 <hr class="featurette-divider">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>© 2017-2018 KUY LAMPUNG, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+      </footer>
+</body>
+</html>
