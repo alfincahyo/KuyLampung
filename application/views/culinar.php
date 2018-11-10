cafc379fb5609b73ac7d6f19036b462ef133bb75
Frisky Riana Kesumadewi <38089317+friskyriana@users.noreply.github.com>
2018-11-10 18:53:58 +0700

    Add files via upload

--- application/views/culinar.php
+++ application/views/culinar.php
@@ -36,7 +36,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
           <img src="<?=base_url()?>assets/img/culinary/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
           <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
-          9.5 / 10
+          9.2 / 10
           </div>
 
           <div class="col-md-5">
@@ -55,12 +55,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
              <br> 
       <br>
       <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
-          <img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>  
-          <img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>  
-          <img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>  
-          <img src="<?=base_url()?>assets/img/balam/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
-          <br><img src="<?=base_url()?>assets/img/balam/none.png"  height="50px" width="50px";> 
-          9.5 / 10
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          9.0 / 10
           </div>
           <div class="col-md-5 order-md-1">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/tempoyak.jpg" data-holder-rendered="true">
@@ -74,6 +74,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7" style="margin-top: 10px;">
             <h2 class="featurette-heading">GULAI TABOH</h2>
             <p class="lead"> Gulai taboh memang dikenal sebagai makanan khas dari Lampung yang memiliki cita rasa yang sangat unik. Gulai taboh ini biasanyan terbuat dari ubi-ubian dan kacang-kacangan. Proses pembuatan gulai taboh terbilang sedikit mudah yakni dengan mencapurkan santan dengan kacang hijau ataupun kacang lainnya, serta juga bisa mencapurkan rebung dan kentang ke dalam masakan ini. Jika ingin membuat rasa yang berbeda maka anda bisa menambahkan daun sirih atau terasi ke dalamnya. Dengan adanya perpaduan antara ubi dengan terasi tersebut dapat membuat makanan terasa lebih lezat dilidah</p>
+          <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          8.7 / 10
           </div>
           <div class="col-md-5">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/gulaitaboh2.jpg" data-holder-rendered="true">
@@ -87,6 +96,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <h2 class="featurette-heading">ENGKAK</h2>
             <p class="lead">Engkak merupakan makanan yang memiliki rasa yang cukup manis. Bahan utama untuk pembuatan engkak ialah telur dan mentega yang dihaluskan menjadi tekstur yang sangat lembek. Engkak ini sangat cocok disantap sebagai makanan ringan dan juga sarapan. Bahan-bahan yang digunakan untuk pembuatan engkak ialah gula pasir, tepung ketan putih, telur ayam, susu kental manis, santan serta mentega.
       Selanjutnya semua bahan tesebut bisa anda campurkan menjadi satu lalu aduk hingga merata. Setelahnya anda bisa memasukan adonan tersebut ke dalam loyang yang sudah dioleskan mentega dan juga di beri alas kertas berih. Anda bisa memanggang kue ini layaknya lapis segit.</p>
+          <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/halfstar.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          8.0 / 10
           </div>
           <div class="col-md-5 order-md-1">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/engkak2.jpg" data-holder-rendered="true">
@@ -99,6 +117,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7" style="margin-top: 10px;">
             <h2 class="featurette-heading">PINDANG </h2>
             <p class="lead"> Pindang merupakan salah satu makanan khas dari kota Lampung yang wajib anda cicipi saat berkunjung ke kota yang penuh dengan pesona yang sangat menakjubkan ini. Pindang bisanya disajikan dengan kuah yang berwarna kuning. Pindang khas Lampung ini memiliki cita rasa yang sangat unik seperti asam gurih dengan aroma yang sangat kuat. Aroma yang kuat ini berasal dari duan kemangi yang dicampurkan ke dalam kuah. </p>
+            <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          9.5 / 10
           </div>
           <div class="col-md-5">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/pindang2.jpg" data-holder-rendered="true">
@@ -112,6 +139,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <h2 class="featurette-heading">PANGGANG LAMPUNG</h2>
             <p class="lead">Panggang merupakan makanan khas dari Lampung yang proses pembuatannya harus diasapi ataupun dipanggang. Sehingga tak heran jika makanan khas Lampung ini dikenal dengan nama panggang.
       Panggang menggunakan ikan sebagai bahan utama dalam pembuatannya yang kemudian di panggang ataupun di asapi di atas bara api. Proses pemanganan ini juga tidak membutuhkan waktu yang lama, hanya saja anda harus menunggu hingga ikannya matang. Apabila ikan tersebut sudah matang maka anda bisa langsung memakannya.</p>
+          <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/halfstar.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          8.0 / 10
           </div>
           <div class="col-md-5 order-md-1">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/ikanpanggang.png" data-holder-rendered="true">
@@ -124,6 +160,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7" style="margin-top: 10px;">
             <h2 class="featurette-heading">PISRO </h2>
             <p class="lead"> Pisro merupakan jenis makanan dari olahan ikan yang sering disajikan sebagai jamuan makan sehari-hari masyarakat Pepadun. Dalam proses pembuatannya pisro menggunakan bahan utama ikan betok dan tembakang. Sementara sayur yang digunakan untuk pembuatan pisro mempunyai perpaduan rasa yang sama, manis, pedas serta gurih. Rasa gurih yang ada pada sayur ini berasal dari terasi ikan.Bahan lainnya yang dapat membuat rasa gurih pada sayur pisro yakni ikan bakar yang ada dalam kuah yang sudah dicampurkan dengan bumbu kental lainnya. Sedangkan rasa manis yang ada pada pisro ini berasal dari gula aren yang telah digiling bersamaan dengan bumbu seperti capai dan lainnya. </p>
+            <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/halfstar.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          8.5 / 10
           </div>
           <div class="col-md-5">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/pisro.jpg" data-holder-rendered="true">
@@ -136,6 +181,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7 order-md-2" style="margin-top: 10px;">
             <h2 class="featurette-heading">SAMBAL TEMPOYAK UDANG</h2>
             <p class="lead"> Bagi anda yang sedang berada di Lampung jangan lupa untuk mencicipi sambal tempoyak udang ini. Karena dijamin anda tidak akan kecewa saat mencicipi sambal yang menawarkan rasa yang sangak maknyus ini. Pada dasarnya sambal tempoyak udang terbuat dari tomat dan cabai serta berbagai tambahan lainnya seperti pete, kunyit serta udang.</p>
+            <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          9.5 / 10
           </div>
           <div class="col-md-5 order-md-1">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/sambaltempoyakudang.jpg" data-holder-rendered="true">
@@ -149,6 +203,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <h2 class="featurette-heading">SAMBAL ASAM KEMBANG </h2>
             <p class="lead"> Sambal asam kembang merupakan salah satu makanan khas Lampung yang memiliki rasa cukup lezat dan nikmat. Jika mendengar namanya pasti anda mengira sambal ini terbuat dari bahan utamanya asam. Namun perlu diketahui, jika dalam proses pembuatan sambal asam kembang ini sama sekali tidak menggunakan asam, akan tetapi terbuat dari terasi yang dicampur dengan buah kemang seperti buah mangga muda.
       Sambal asam kembang ini juga bisa disantap dengan makanan khas dari kota Lampung lainnya. Selain itu, anda juga bisa menjadikan sambal asam kembang ini sebagai oleh-oleh dari kota Lampung. </p>
+      <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          8.2 / 10
           </div>
           <div class="col-md-5">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 380px;" src="<?=base_url()?>assets/img/culinary/sambalasamkembang.jpg" data-holder-rendered="true">
@@ -161,6 +224,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           <div class="col-md-7 order-md-2" style="margin-top: 10px;">
             <h2 class="featurette-heading">KERIPIK NANGKA</h2>
             <p class="lead"> Selain keripik pisang, ada juga makanan ringan khas Lampung lainnya yang bisa anda jadikan sebagai oleh-oleh yakni keripik nangka. Keripik nangka dengan keripik pisang memang tidak ada bedanya, karena sama-sama enak pada saat disantap. Bagi anda yang suka ngemil dan makanan ringan, patut mencoba keripik nangka khas Lampung ini. Karena pada saat anda mencoba keripik nangka ini dijamin anda akan selalu ketagihan dengan rasa yang dimilikinya. </p>
+            <br> 
+      <br>
+      <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/star.png" alt="bintang" height="25px" width="25px";>  
+          <img src="<?=base_url()?>assets/img/culinary/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/culinary/none.png"  height="50px" width="50px";> 
+          9.0 / 10
           </div>
           <div class="col-md-5 order-md-1">
             <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/keripiknangka.jpg" data-holder-rendered="true">
