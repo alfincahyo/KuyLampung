c3946b43b105150563733509407a76e178838603
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-10 07:38:55 +0700

    Add files via upload

---
+++ application/views/admin/layout/header.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link href="<?=base_url()?>assets/css/header.css" rel="stylesheet" type="text/css">
+</head> 
+<body>
+    <header>
+	  <div class="akun"><a href="<?php echo base_url(); ?>admin/dasbor/config" title="Update konfigurasi"><img src="<?php echo base_url(); ?>assets/images/javawebmedia.png" width="16" height="16"> Sistem Informasi</a></div>
+	  <div class="akun"><a href="<?php echo base_url(); ?>admin/update" title="Update profil"><img src="<?php echo base_url(); ?>assets/images/img/user_red.png" width="16" height="16"> Administrator</a></div>
+      <div class="akun"><a href="<?php echo base_url(); ?>admin/logout" title="Logout disini"><img src="<?php echo base_url(); ?>assets/images/img/delete.png" width="16" height="16"> Logout</a></div>
+	  <div class="home"><a href="<?php echo base_url(); ?>" title="Beranda"><img src="<?php echo base_url(); ?>assets/images/img/house_go.png" width="16" height="16"> Beranda</a></div>
+  </header>
+
+
+        
+</body>    
+</html>
