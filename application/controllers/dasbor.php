db051a97a9d44a18243cd66036458dc0e07d606f
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-10 07:12:35 +0700

    dasbor.php

---
+++ application/controllers/dasbor.php
@@ -0,0 +1,10 @@
+<?php 
+
+if ( !defined('BASEPATH')) exit('No direct script access allowed');
+
+	class Dasbor extends CI_Controller {
+		public function index() {
+			$data=array('title'=>'Halaman Administrator','isi' =>'admin/dasbor/dasbor_view');
+			$this->load->view('admin/layout/wrapper',$data);
+		}
+	}
