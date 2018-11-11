47333063dd824c7ea96c7d082d483c7d069d08a1
alfincahyo <38088162+alfincahyo@users.noreply.github.com>
2018-11-11 20:18:40 +0700

    Add files via upload

---
+++ application/views/souvenirs.php
@@ -0,0 +1,408 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Souveniers</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/oleh.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+<div class ='container'>
+	<div class="garis">
+	<div class=head>
+		OLEH-OLEH KHAS LAMPUNG
+	</div>
+	</div>
+  <div id="demo" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ul class="carousel-indicators">
+		<li data-target="#demo" data-slide-to="0" class="active"></li>
+		<li data-target="#demo" data-slide-to="1"></li>
+		<li data-target="#demo" data-slide-to="2"></li>
+		<li data-target="#demo" data-slide-to="3"></li>
+		<li data-target="#demo" data-slide-to="4"></li>
+		<li data-target="#demo" data-slide-to="5"></li>
+	</ul>
+
+	
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" >
+      <div class="carousel-item active">
+        <img src="<?=base_url()?>assets/img/souveniers/satu.jpg" alt="Banana Foster" height="400px" width="1300px";>
+        <div class="carousel-caption">
+        <h3>Banana Foster</h3>
+        <p>Cake pisang masa kini</p>
+      	</div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="<?=base_url()?>assets/img/souveniers/dua.jpg" alt="Pie Pisang" height="400px" width="1300px";>
+		<div class="carousel-caption" >
+        <h3>Pie Pisang</h3>
+        <p>Gurihnya pie dicampur dengan manisnya pisang</p>
+      	</div>
+	  </div>
+    
+      <div class="carousel-item">
+        <img src="<?=base_url()?>assets/img/souveniers/tiga.jpg" alt="Keripik pisang" height="400px" width="1300px";>
+		<div class="carousel-caption">
+        <h3>Keripik pisang</h3>
+        <p>Aneka rasa sesuai selera anda</p>
+      	</div>
+	  </div>
+
+	  <div class="carousel-item">
+        <img src="<?=base_url()?>assets/img/souveniers/empat.jpg" alt="Kopi Lampung" height="400px" width="1300px";>
+		<div class="carousel-caption">
+        <h3>Kopi Lampung</h3>
+        <p>Cita rasa kopi khas lampung</p>
+      	</div>
+	  </div>
+
+	   <div class="carousel-item">
+        <img src="<?=base_url()?>assets/img/souveniers/lima.jpg" alt="Sambal Lampung" height="400px" width="1300px";>
+		<div class="carousel-caption">
+        <h3>Sambal Lampung</h3>
+        <p>Pedas gurihnya bikin nagih</p>
+      	</div>
+	  </div>
+
+	   <div class="carousel-item">
+        <img src="<?=base_url()?>assets/img/souveniers/enam.jpg" alt="Miniatur siger" height="400px" width="1300px";>
+		<div class="carousel-caption">
+        <h3>Miniatur Siger</h3>
+        <p>Hiasan cantik dari lampung</p>
+      	</div>
+	  </div>
+    </div>
+
+
+    <!-- Left and right controls -->
+	<a class="carousel-control-prev" href="#demo" data-slide="prev">
+		<span class="carousel-control-prev-icon"></span>
+	</a>
+	<a class="carousel-control-next" href="#demo" data-slide="next">
+		<span class="carousel-control-next-icon"></span>
+	</a>
+  </div>
+
+	<div class="row">
+	<br>
+		<div class="col-8 " >
+			<div class="judul">
+				<p><h3>Lampung Banana Foster</h3></p> <br>
+			</div>
+			
+			<p> 
+            Banana Foster kini hadir di Lampung yang mengusung cake berbahan dasar pisang yang menggunakan tagline oleh-oleh kekinian khas Lampung. 
+            Hengky kurniawan bersama tim nya membuka galeri banana foster lampung beralamat di Jln. 
+            Wolter Mongosidi No.115, anda bisa mencicipi cake pisang dengan 5 pilihan rasa.
+			</p> <br><br>
+			<div class="row">
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/money.png" alt="uang" height="50x" width="50px";>	
+					<br><br>
+					40.000 - 60.000
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>	
+					<br><img src="<?=base_url()?>assets/img/souveniers/none.png"  height="50px" width="50px";>	
+					<br>
+					7.9 / 10
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/mytime.png" alt="waktu" height="50x" width="50px";>		
+					<br><br>
+					07.00 - 20.00
+				</div>
+			</div>
+		</div>
+		<div class="col ">
+		<br>
+			<div class="card" >
+			<img src="<?=base_url()?>assets/img/souveniers/isi1.jpg" class="img-thumbnail" alt="Cinque Terre" width="100%" ;>
+				<div class="card-body">
+				<h4 class="card-title" >Banana Foster</h4>
+				<p class="card-text" >Dengan berbagai varian rasa olahan pisang yang unik akan membuat lidah anda bergoyang</p>
+				<a href=https://lampungbananafoster.com/ target="_top" class="btn btn-primary" >go website</a>
+				</div>
+			</div>
+			<br>
+		</div><br>
+	</div>
+
+
+	<div class="row">
+		<div class="col ">
+		<div class="card" >
+			<img src="<?=base_url()?>assets/img/souveniers/isi2.jpg" class="img-thumbnail" alt="Cinque Terre" width="100%" ;>
+				<div class="card-body">
+				<h4 class="card-title" >Pie Pisang Yussy Akmal</h4>
+				<p class="card-text" >Gurihnya pie dan manisnya pisang akan menggugah selera anda</p>
+				<a href=https://yussyakmal.com/ target="_top" class="btn btn-primary" >go website</a>
+				</div>
+			</div>
+			<br>
+		</div>
+		<div class="col-8 " >
+			<div class="judul">
+				<p><h3>Pie Pisang Yussy Akmal</h3></p>
+			</div>
+		
+			<p> <br>
+            Selain keripik, pie pisang juga bisa jadi pilihan buah tangan yang lezat.
+            Yussy Akmal termasuk toko roti dan kue populer di Bandar Lampung.
+            Ada tiga cabang yang salah satunya berlokasi di Jalan Jendral Sudirman No 17A, Kota Bandar Lampung, Provinsi Lampung.
+            Toko yang mulai beroperasi sekitar tahun 2010 itu mengklaim sebagai pelopor pie pisang Lampung.
+            Olahan kuliner berbahan dasar pisang yang kini menjadi buah tangan favorit. Penganan tersebut adalah pie susu pisang.
+			</p><br><br>
+			<div class="row">
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/money.png" alt="uang" height="50x" width="50px";>
+					<br><br>
+					50.000 - 100.000
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>	
+					<br><img src="<?=base_url()?>assets/img/souveniers/none.png"  height="50px" width="50px";>	
+					<br>
+					8.7 / 10
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/mytime.png" alt="waktu" height="50x" width="50px";>
+					<br><br>
+					07.00 - 18.00
+				</div>
+			</div>
+		</div><br>
+	</div>
+
+
+	<div class="row">
+		<div class="col-8 " >
+			<div class="judul">
+				<p><h3>Keripik Pisang</h3></p>
+			</div>
+			
+			<p> <br>
+			Keripik pisang kini telah identik sebagai penganan khas sekaligus oleh-oleh dari Provinsi Lampung. 
+			Varian rasanya yang beragam dengan tekstur yang renyah, membuat keripik pisang Lampung dicari dan dinanti.
+			Bagi anda yang bertandang ke Bandar Lampung, Jalan Pagar Alam wajib hukumnya untuk masuk ke rute perjalanan anda. 
+			Mengapa demikian? Karena di lokasi ini lah anda akan merasa berada di surga keripik pisang. 
+			Jalan ini merupakan satu-satunya jalan yang didedikasikan untuk sentra penjualan keripik pisang di Kota Tapis Berseri.
+			Artikel ini telah tayang di tribunlampung.co.id dengan judul Gang PU, Surganya Keripik Pisang Aneka Rasa Khas Lampung,
+			</p><br><br>
+			<div class="row">
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/money.png" alt="uang" height="50x" width="50px";>
+					<br><br>
+					40.000 - 60.000
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>	
+					<br><img src="<?=base_url()?>assets/img/souveniers/none.png"  height="50px" width="50px";>	
+					<br>
+					8.3 / 10
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/mytime.png" alt="waktu" height="50x" width="50px";>
+					<br><br>
+					07.00 - 20.00
+				</div>
+			</div>
+		</div>
+		<div class="col ">
+			<div class="card" >
+			<img src="<?=base_url()?>assets/img/souveniers/isi3.jpg" class="img-thumbnail" alt="Cinque Terre" width="100%" ;>
+				<div class="card-body">
+				<h4 class="card-title" >Keripik pisang</h4>
+				<p class="card-text" >Keripik renyah dengan berbagai macam toping beraneka rasa</p>
+				<a href=https://shopee.co.id/search?keyword=keripik%20pisang target="_top" class="btn btn-primary" >go website</a>
+				</div>
+			</div>
+			<br>
+		</div><br>
+	</div>
+
+
+	<div class="row">
+		<div class="col ">
+		<div class="card" style="width:350px">
+			<img src="<?=base_url()?>assets/img/souveniers/isi4.jpg" class="img-thumbnail" alt="Cinque Terre" width="100%" ;>
+				<div class="card-body">
+				<h4 class="card-title" style="text-align:center;">Kopi Lampung</h4>
+				<p class="card-text" style="text-align:center;">Nikmati kopi lampung yang bercita rasa khas</p>
+				<a href=https://www.bukalapak.com/products/s/kopi-lampung target="_top" class="btn btn-primary" >go website</a>
+				</div>
+			</div>
+			<br>
+		</div>
+		<div class="col-8 " >
+			<div class="judul">
+				<p><h3>Kopi Lampung</h3></p>
+			</div>
+		
+			<p> <br>
+			kopi ini memiliki keistimewaan yang berbeda dari aroma serta cita rasanya yang sangat khas. 
+			Para petani kopi yang ada di kota lampung paling banyak membudidayakan jenis kopi robusta di lahannya. 
+			Diketahui bahwa sebagian besar lahan kebun kopi yang ada pada lahan dataran tinggi di lampung tersebut 
+			adalah kebun milik rakyat yang terpusat di daerah lampung tengah serta lampung barat dan begitu juga dengan daerah tanggamus. 
+			</p> <br><br>
+			<div class="row">
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/money.png" alt="uang" height="50x" width="50px";>
+					<br><br>
+					20.000 - 100.000
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>	
+					<br><img src="<?=base_url()?>assets/img/souveniers/none.png"  height="50px" width="50px";>	
+					<br>
+					8.7 / 10
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/mytime.png" alt="waktu" height="50x" width="50px";>
+					<br><br>
+					07.00 - 18.00
+				</div>
+			</div>
+		</div><br>
+	</div>
+
+	
+	<div class="row">
+		<div class="col-8 "  >
+			<div class="judul">
+				<p><h3>Sambal Lampung Yen-yen </h3></p>
+			</div>
+			
+			<p> <br>
+			Sambal Yen yen dibuat dari cabe caplak segar, tanpa campuran ...ditambah gula, cuka dan bawang putih... 
+			Pedas nya boleh diacungkan jempol. Disarankan kalau sudah terbuka, di simpan dalam lemari es. 
+			Biasa nya sambel ini cocok buat makan seafood, bakso, atau juga mie ayam, mie kuah, bisa juga buat yang lain, 
+			karena rasa super pedas dan ada rasa asam-asam nya.
+			</p> <br><br>
+			<div class="row">
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/money.png" alt="uang" height="50x" width="50px";>
+					<br><br>
+					40.000 - 60.000
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>	
+					<br><img src="<?=base_url()?>assets/img/souveniers/none.png"  height="50px" width="50px";>	
+					<br>
+					8.3 / 10
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/mytime.png" alt="waktu" height="50x" width="50px";>
+					<br><br>
+					07.00 - 20.00
+				</div>
+			</div>
+		</div>
+		<div class="col ">
+			<div class="card" >
+			<img src="<?=base_url()?>assets/img/souveniers/isi5.png" class="img-thumbnail" alt="Cinque Terre" width="100%" ;>
+				<div class="card-body">
+				<h4 class="card-title" >Sambal Lampung yen-yen</h4>
+				<p class="card-text" >Pedasnya sambal dengan perpaduan unik yang bikin mata melek</p>
+				<a href=http://www.iyenfoods.com/sambal-lampung-yenyen-iyen target="_top" class="btn btn-primary" >go website</a>
+				</div>
+			</div>
+			<br>
+		</div><br>
+	</div>
+
+
+	<div class="row">
+		<div class="col ">
+		<div class="card" >
+			<img src="<?=base_url()?>assets/img/souveniers/isi6.jpg" class="img-thumbnail" alt="Cinque Terre" width="100%" ;>
+				<div class="card-body">
+				<h4 class="card-title" >Miniatur Siger Lampung</h4>
+				<p class="card-text" >Bentuk miniatur siger yang cocok dijadikan pajangan rumah anda</p>
+				<a href=https://www.mahkotasouvenir.com/2015_09_20_archive.html target="_top" class="btn btn-primary" >go website</a>
+				</div>
+			</div>
+			<br>
+		</div>
+		<div class="col-8 " >
+			<div class="judul">
+				<p><h3>Miniatur Siger Lampung</h3></p>
+			</div>
+		
+			<p> <br>
+            MINIATUR MENARA SIGER LAMPUNG
+            SOUVENIR KHAS LAMPUNG
+            Cocok Untuk Souvenir:
+            Kunjungan, Pemateri, Pembicara, PKL, Study Tour atau Hiasan Rumah/Kantor
+            Harga Rp.180.000,-/Pcs
+            TIDAK ADA MINIMAL PEMESANAN, PESAN 1 BISA
+            Harga Sudah termasuk Cetak Tulisan/Begdrop dan Paper Bag
+
+            Bahan Fiber, Panjang 26cm, Lebar 10,5cm, Tinggi 20cm
+            Box Kaca Acrilic 2mm
+			</p><br><br>
+			<div class="row">
+				<div class="col-sm-4">
+					<img src="<?=base_url()?>assets/img/souveniers/money.png" alt="uang" height="50x" width="50px";>
+					<br><br>
+					185.000
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/souveniers/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>	
+					<br><img src="<?=base_url()?>assets/img/souveniers/none.png"  height="50px" width="50px";>	
+					<br>
+					8.0 / 10
+				</div>
+				<div class="col-sm-4" >
+					<img src="<?=base_url()?>assets/img/souveniers/mytime.png" alt="waktu" height="50x" width="50px";>
+					<br><br>
+					07.00 - 18.00
+				</div>
+			</div>
+		</div><br>
+	</div>
+
+
+</div>
+</body>
+</html>
