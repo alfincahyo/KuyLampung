e7649f147674a2ee2c1f2cfc22a31e1897281d8f
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-13 02:16:56 +0700

    Update footer.php

--- application/views/admin/layout/footer.php
+++ application/views/admin/layout/footer.php
@@ -1,24 +1,35 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+</div>
+</div>
+</div>
+<!--  end  Context Classes  -->
+</div>
+</div>
+<!-- /. ROW  -->
+</div>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
-    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/header.css">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-</head>
+</div>
+<!-- /. PAGE INNER  -->
+</div>
+<!-- /. PAGE WRAPPER  -->
+<!-- /. WRAPPER  -->
+<!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+<!-- JQUERY SCRIPTS -->
+<script src="<?php echo base_url() ?>assets/admin/assets/js/jquery-1.10.2.js"></script>
+<!-- BOOTSTRAP SCRIPTS -->
+<script src="<?php echo base_url() ?>assets/admin/assets/js/bootstrap.min.js"></script>
+<!-- METISMENU SCRIPTS -->
+<script src="<?php echo base_url() ?>assets/admin/assets/js/jquery.metisMenu.js"></script>
+<!-- DATA TABLE SCRIPTS -->
+<script src="<?php echo base_url() ?>assets/admin/assets/js/dataTables/jquery.dataTables.js"></script>
+<script src="<?php echo base_url() ?>assets/admin/assets/js/dataTables/dataTables.bootstrap.js"></script>
+<script>
+$(document).ready(function () {
+$('#dataTables-example').dataTable();
+});
+</script>
+<!-- CUSTOM SCRIPTS -->
+<script src="<?php echo base_url() ?>assets/admin/assets/js/custom.js"></script>
 
-<body>
 
-	<footer class="container" style="margin-top: 20px; border: 2px;">
-		 <hr class="featurette-divider">
-        <p class="float-right"><a href="#">Back to top</a></p>
-        <p>© 2017-2018 KUY LAMPUNG, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-      </footer>
 </body>
 </html>
