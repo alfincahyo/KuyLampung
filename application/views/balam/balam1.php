4d690b00dac94791395139c1933a8a47ccf6f135
Annisa Gita Asmara <38088770+aannisagita@users.noreply.github.com>
2018-11-16 22:29:14 +0700

    Add files via upload

---
+++ application/views/balam/balam1.php
@@ -0,0 +1,150 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PANTAI MUTUN</title>
+   <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/komen.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/balam.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+<div class ='container'>
+    <div class="head">
+    BANDAR LAMPUNG
+    </div>
+    <div class="row">
+        <div class="col-8 " >
+            <div class="judul">
+            <h2>EKSOTISME PANTAI MUTUN DAN PULAU TANGKIL</h2>
+            <img src="<?=base_url()?>assets/img/balam/mutun3.jpg" class="img-thumbnail" alt="Cinque Terre" height="410px" width="820px";>	
+            </div>
+            <div class="isi">
+            Pantai Mutun dan Pulau Tangkil merupakan salah satu Tempat Wisata Menawan yang terletak di Provinsi Lampung.  
+            Lokasinya tak jauh dari Kota Bandar Lampung,hanya sekitar 10 Km dari pusat kota menuju ke arah kecamatan 
+            Padang Cermin Kabupaten Pesawaran.  Pantai Mutun ini termasuk destinasi wisata favorit warga Bandarlampung dan sekitarnya.  
+            Berita bagusnya, pengelolaan pantai mutun ini mulai dibenahi, dibeberapa sudut mulai dibangun berbagai fasilitas 
+            untuk memanjakan para wisatawan.
+            <br><br>
+            Harga tiket masuk ke Pantai Mutun cukup terjangkau, untuk masuk kawasan ini, setiap pengunjung dikenakan biaya Rp 10.000/orang, 
+            sedangkan untuk  parkir Rp. 10.000/motor, dan Rp. 15.000 / mobil. Kalau nggak pengen kepanasan, kita bisa sewa pondokan yang 
+            tersedia di tepi Pantai Mutun.  Pondok-pondokan bambu disewakan dengan harga sekitar Rp. 25 ribu – Rp. 50 ribu tergantung ukurannya,
+            waktu, apakah pas libur weekend atau pas libur besar nasional, dan terkadang tergantung kelihaian kita bernegosiasi.
+            Pantai Mutun juga sudah ada penyewaan berbagai permainan air, seperti : Cano, tarifnya sekitar Rp. 15.000,- , Banana Boat  
+            Rp. 35.000,-/orang,  Water Boom Rp. 25.000,-,  Wahana Air Rp.25.000-35.000,-.   Harga makanan di cafe restauran berkisar Rp. 10.000 – 50.000,- . (Catatan : Harga tercantum di atas adalah harga pada tahun 2012/2013).
+            <br><br>SARANA YANG ADA DI PANTAI MUTUN LAMPUNG<br><br>
+            Sebagai mana tertulis di atas, Pantai Mutun Lampung sudah dilengkap dengan akomodasi yang lumayan lengkap untuk wisata keluarga, 
+            seperti : Pondokan, Perahu Cano, Water boom, Banana Boat, dan restauran.
+            Kalau pengen suasana tenang dan sepi saat wisata ke Pantai Mutun, kita sebaiknya datang lebih pagi, 
+            sebab kalau sore makin rame pengunjungnya.  Kalau sepi, berenangnya nggak umpel-umpelan, dan bisa khusuk nikmatin keindahan pantai.  
+            Pantai Mutun merupakan pantai teluk, sehingga  ombaknya yang tidak terlalu tinggi, cocok dan aman untuk rekreasi keluarga, 
+            termasuk anak-anak.  Pasir yang putih, bersih dan air lautnya jernih banget, bikin liburan atau refreshing.
+            <br><br>MENYEBERANG DARI PANTAI MUTUN KE PULAU TANGKIL<br><br>
+            Kalau sudah sampai di Pantai Mutun, nggak ada salahnya sewa perahu menuju Pulau Tangkil, 
+            pulau tak berpenghuni yang pantainya indah.  Jarak tempuh pantai Mutun – Pulau tangkil sekitar 10 menit perjalanan, 
+            sewa perahu PP berkisar 60 – 100ribu/perahu atau sekitar 15.000 – 20.000/orang, bisa muat hingga 10 orang/perahunya.
+            Pulau tangkil memiliki pantai berkontur landai, dengan hamparan pasir yang bersih.  Cocok untuk dinikmati bersama keluarga tercinta, termasuk anak-anak, atau bersama para sahabat.
+            </div>
+            <div class="komentar">
+            <?php
+                include "formkomentar.php";
+            ?>
+            </div>
+            
+        </div>
+        <div class="col-4 ">
+        <div id="sidebar" >
+            <div id="clock" class="clock2"></div>
+            <script type="text/javascript">
+
+                function showTime(){
+                    var date = new Date();
+                    var h = date.getHours();
+                    var m = date.getMinutes();
+                    var s = date.getSeconds();
+                    var session = "A.M";
+                    
+                    if(h==0){
+                        h=12;
+                    }
+                    if(h>12){
+                        h= h-12;
+                        session= "P.M";
+                    }
+
+                    h = (h<10) ? "0" + h : h;
+                    m = (m<10) ? "0" + m : m;
+                    s = (s<10) ? "0" + s : s;
+                
+                    var time = h + ":" + m + ":" + s + " " + session;
+                    document.getElementById("clock").innerText = time;
+                    document.getElementById("clock").textContent = time;
+
+                    setTimeout(showTime, 1000);
+                
+                
+                }
+                    showTime();
+                </script>
+                
+            <div class="judulwidget">ARTIKEL MENARIK LAINNYA<br></div>
+
+                <div class="widget">
+                        <ul>
+                            <p><li><a href="<?php echo site_url('balam/balam1'); ?>">Pantai Mutun</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam2'); ?>">Puncak Mas</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam3'); ?>">Taman kupu-kupu</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam4'); ?>">Museum Lampung</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam5'); ?>">Horti Park</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam6'); ?>">Bukit Sakura</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam7'); ?>">Lembah Hijau</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam8'); ?>">Alam Wawai</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam9'); ?>">Hutan Pinus (Zona 235)</a></li></p>
+                            <p><li><a href="<?php echo site_url('balam/balam10'); ?>">Penangkaran Rusa</a></li></p>
+                        </ul>
+                </div>
+                <div class="nilai" style="text-align:center;">
+					<p><img src="<?=base_url()?>assets/img/balam/money.png" alt="uang" height="50x" width="50px";>	
+					<br><br>
+                    80.000 - 120.000
+                    </p>                
+                </div>
+                <div class="nilai" style="text-align:center;">
+                    <p>
+                    <img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/balam/star.png" alt="bintang" height="25px" width="25px";>	
+					<img src="<?=base_url()?>assets/img/balam/emptystar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>	
+					<br><img src="<?=base_url()?>assets/img/balam/none.png"  height="50px" width="50px";>	
+					<br>
+                    7.5 / 10
+                    </p>                   
+                </div>
+                <div class="nilai" style="text-align:center;">
+                    <p>
+                    <img src="<?=base_url()?>assets/img/balam/mytime.png" alt="waktu" height="50x" width="50px";>		
+					<br><br>
+                    Setiap Hari <br> 07.00 - 20.00
+                    </p>                    
+				</div>
+        </div>
+		<br>
+        </div>
+    </div>
+        
+</div>
+</body>
+</html>
