5fdb31ce4c3f4f41ba5c7d5e363e428544b36f2f
alfincahyo <38088162+alfincahyo@users.noreply.github.com>
2018-11-17 23:22:42 +0700

    Add files via upload

---
+++ application/models/admin_model.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class admin_model extends CI_Model {
+
+		public function __construct(){
+        $this->load->database();
+	  }
+
+    public function listing(){
+        $query = $this->db->get('admin');
+        return $query->result();
+    }
+
+    public function detail($id_daerah){
+        $query = $this->db->get_where('admin',array('id_admin' => $id_admin));
+        return $query->row();
+    }
+
+    public function tambah($data){
+        $this->db->insert('admin',$data);
+    }
+
+    public function edit($data){
+        $this->db->where('id_admin',$data['id_admin']);
+        $this->db->update('admin',$data);
+    }
+
+    public function hapus($data){
+        $this->db->where('id_admin',$data['id_admin']);
+        $this->db->delete('admin',$data);
+    }
+}
