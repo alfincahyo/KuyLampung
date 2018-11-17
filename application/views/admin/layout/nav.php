15d5476e6b228454fa00fda5cbfd898e518d2ec4
alfincahyo <38088162+alfincahyo@users.noreply.github.com>
2018-11-17 23:26:21 +0700

    Add files via upload

--- application/views/admin/layout/nav.php
+++ application/views/admin/layout/nav.php
@@ -2,12 +2,33 @@
 <nav class="navbar-default navbar-side" role="navigation">
   <div class="sidebar-collapse">
     <ul class="nav" id="main-menu">
-      <li><a href="#"><i class="fa fa-user"></i> Administrator<span class="fa arrow"></span></a>
+      <li><a href="#"><i class="fa fa-user"></i> Daerah<span class="fa arrow"></span></a>
         <ul class="nav nav-second-level">
-          <li><a href="<?php echo base_url('admin/user') ?>">Data Admin</a></li>
-          <li><a href="<?php echo base_url('admin/user/tambah') ?>">Tambah Admin</a></li>
+          <li><a href="<?php echo base_url('admin/daerah') ?>">Data Daerah</a></li>
         </ul>
       </li>
+
+      <li><a href="#"><i class="fa fa-user"></i> Artikel<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="<?php echo base_url('admin/artikel') ?>">Data Artikel</a></li>
+          <li><a href="<?php echo base_url('admin/artikel/tambah') ?>">Tambah Artikel</a></li>
+        </ul>
+      </li>
+
+      <li><a href="#"><i class="fa fa-user"></i> Komentar<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="<?php echo base_url('admin/komentar') ?>">Data Komentar</a></li>
+        </ul>
+      </li>
+
+      <li><a href="#"><i class="fa fa-user"></i>Admin<span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="<?php echo base_url('admin/data_admin') ?>">Data Admin</a></li>
+
+
+        </ul>
+      </li>
+
     </ul>
  </div>
 </nav>
