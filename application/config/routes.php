48ce6281344472f6bc5b56cbf0ebb33e5a79c279
alfincahyo <38088162+alfincahyo@users.noreply.github.com>
2018-11-17 17:08:00 +0700

    Add files via upload

--- application/config/routes.php
+++ application/config/routes.php
@@ -58,7 +58,7 @@ $route['location'] = 'home/location';
 $route['souvenirs'] = 'home/souvenirs';
 $route['culinar'] = 'home/culinar';
 $route['about_us'] = 'home/about_us';
-$route['login'] = 'home/login';
+$route['login'] = 'Login/index';
 //location
 $route['balam'] = 'location/balam';
 $route['lamsel'] = 'location/lamsel';
