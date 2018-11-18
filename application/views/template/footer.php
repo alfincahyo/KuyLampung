3815e7750dc176f8d1f18e455a183107ab96c6e4
Frisky Riana Kesumadewi <38089317+friskyriana@users.noreply.github.com>
2018-11-18 21:08:19 +0700

    Add files via upload

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -15,10 +15,129 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <body>
 
-	<footer class="container" style="margin-top: 20px; border: 2px;">
-		 <hr class="featurette-divider">
-        <p class="float-right"><a href="#">Back to top</a></p>
-        <p>© 2017-2018 KUY LAMPUNG, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-      </footer>
-</body>
-</html>
+	<footer class="page-footer font-small blue-grey lighten-5">
+	<div class=" class="container" style="margin-top: 50px; border: 20px;">
+	
+
+    <div class="container">
+
+	 <!-- row-->
+	 <div class="row py-4 d-flex align-items-center">
+
+	 <!-- column -->
+	 <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+            <h4 class="mb-0">us on social networks! </h4>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+    <!-- Link -->
+    <div class="container text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+      <div class="row mt-3 dark-grey-text">
+
+     <!-- Grid column -->
+       <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+
+       	<br>
+
+       	<!-- Content -->
+          <h6 class="text-uppercase font-weight-bold">Company name</h6>
+          <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>Kami dari kelompok DUDUDU membuat web pariwisata. </p>
+          <p>Semoga membantu!</p>
+          </div>
+
+          <!-- column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        	<br>
+
+         <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Products</h6>
+          <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+
+           <p>
+           	<a class="dark-grey-text" href="#!">MENU</a>
+          </p>
+
+          <p>
+            <a class="dark-grey-text" href="#!">Location</a>
+          </p>
+
+          <p>
+            <a class="dark-grey-text" href="#!">Culinary</a>
+          </p>
+
+          <p>
+            <a class="dark-grey-text" href="#!">Souvenirs</a>
+          </p>
+
+        </div>
+
+        <br>
+
+        <!-- column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        	<br>
+
+        	<!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">XXXXX</h6>
+          <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+
+          <p>
+            <a class="dark-grey-text" href="#!">XXXXX</a>
+          </p>
+
+          <p>
+            <a class="dark-grey-text" href="#!">XXXXX</a>
+          </p>
+
+          <p>
+            <a class="dark-grey-text" href="#!">SXXXX</a>
+          </p>
+
+          <p>
+            <a class="dark-grey-text" href="#!">XXXXX</a>
+          </p>
+
+        </div>
+
+         <!-- column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+        	<br>
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Contact</h6>
+          <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <i class="fa fa-home mr-3"></i> Lampung Selatan </p>
+          <p>
+            <i class="fa fa-envelope mr-3"></i> KuyLampung.com</p>
+          <p>
+            <i class="fa fa-phone mr-3"></i> 085789943472 </p>
+          <p>
+            <i class="fa fa-print mr-3"></i> 0721 - 888888 </p>
+
+        </div>
+
+        </div>
+
+        </div>
+
+        </div>
+
+        <!-- Copyright -->
+    	<div class="footer-copyright text-center text-black-50 py-3">© 2018 Copyright:
+     	 <a class="dark-grey-text"> KuyLampung.com</a>
+     	 </div>
+
+     	 </footer>
+		</body>
+		</html>
