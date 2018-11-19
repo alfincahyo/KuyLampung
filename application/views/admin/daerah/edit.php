43c66f6f23c47453c91409eaf859ed5c118293b1
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-19 23:23:50 +0700

    Update edit.php

--- application/views/admin/daerah/edit.php
+++ application/views/admin/daerah/edit.php
@@ -1,30 +1,43 @@
 <?php
 echo validation_errors('<div class="alert alert-warning">,</div>');
 
-echo form_open(base_url('admin/daerah/edit/'.$daerah->id_daerah));
+echo form_open_multipart(base_url('admin/daerah/edit/'.$daerah->id_daerah));
 
 ?>
 
-<div class="col-md-4">
+<div class="col-md-6">
   <div class="form-group form-group-lg">
     <label>Nama Daerah</label>
-    <input type="text" name="nama_daerah" class"form-control" placeholder="Nama Daerah" value="
-        <?php
-          echo $daerah->nama_daerah ?>" required>
+    <input type="text" name="nama_daerah" class"form-control" placeholder="Nama Daerah" value="<?php echo $daerah->nama_daerah ?>" required>
+  </div>
+</div>
+
+<div class="col-md-6">
+  <div class="form-group form-group-lg">
+    <label>Alamat</label>
+    <input type="text" name="alamat" class"form-control" placeholder="Masukkan Alamat" value="<?php echo $daerah->alamat ?>" required>
+  </div>
+</div>
+
+<div class="col-md-6">
+  <div class="form-group form-group-lg">
+    <label>Nomor Telepon</label>
+    <input type="text" name="telepon" class"form-control" placeholder="Nomor Telepon" value="<?php echo $daerah->telepon ?>"required>
   </div>
 </div>
 
 <div class="col-md-3">
   <div class="form-group form-group-lg">
-    <label>Alamat</label>
-    <input type="text" name="alamat" class"form-control" placeholder="Masukkan Alamat" value="
-        <?php
-          echo $daerah->alamat ?>" required>
+    <label>Logo Daerah</label>
+    <input type="file" name="logo_daerah" class"form-control" required>
   </div>
 </div>
-<div class="form-group">
-  <input type="submit" name="submit" class="btn btn-primary btn-lg" value="Save">
-  <input type="reset" name="reset" class="btn btn-default btn-lg" value="Reset">
+
+<div class="col-md-6">
+  <div class="form-group">
+    <input type="submit" name="submit" class="btn btn-primary btn-lg" value="Save">
+    <input type="reset" name="reset" class="btn btn-default btn-lg" value="Reset">
+  </div>
 </div>
 
 <?php echo form_close(); ?>
