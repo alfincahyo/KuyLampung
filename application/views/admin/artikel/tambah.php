aa6024456127237b266f52c4e16fadf2b40ceaee
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-19 23:19:35 +0700

    Create tambah.php

---
+++ application/views/admin/artikel/tambah.php
@@ -0,0 +1,46 @@
+<script src="<?php echo base_url('assets/tinymce/js/tinymce/tinymce.min.js') ?>" type="text/javascript"></script>
+<script>tinymce.init({ selector:'textarea'});</script>
+
+<?php
+  echo validation_errors('<div class="alert alert-warning">,</div>');
+
+  echo form_open(base_url('admin/artikel/tambah'));
+?>
+
+    <div class="col-md-8">
+      <div class="form-group form-group-lg">
+        <label>Pilih Daerah</label>
+        <select class="form-control" name="id_daerah">
+          <?php foreach ($daerah as $daerah) { ?>
+            <option value="<?php echo $daerah->id_daerah ?>">
+              <?php echo $daerah->nama_daerah ?>
+            </option>
+          <?php  } ?>
+        </select>
+      </div>
+    </div>
+
+  <div class="col-md-8">
+    <div class="form-group form-group-lg">
+      <label>Judul Artikel</label>
+      <input type="text" name="judul" class"form-control" placeholder="Masukkan Judul" required>
+    </div>
+  </div>
+
+  <div class="col-md-8">
+    <div class="form-group">
+      <label>Isi</label>
+      <textarea name="isi" class="form-control" placeholder="Isi Artikel" rows="8" cols="80" ></textarea>
+    </div>
+  </div>
+
+ <div class="col-md-8">
+  <div class="form-group form-group-lg">
+    <input type="submit" name="submit" class="btn btn-primary btn-lg" value="Save">
+    <input type="reset" name="reset" class="btn btn-default btn-lg" value="Reset">
+  </div>
+</div>
+
+    <?php
+    echo form_close();
+    ?>
