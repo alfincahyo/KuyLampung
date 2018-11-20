f5bc22b2e60272422ff89bfc3c67fbac3a20a7ab
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-20 21:29:15 +0700

    Update edit.php

--- application/views/admin/daerah/edit.php
+++ application/views/admin/daerah/edit.php
@@ -1,5 +1,5 @@
 <?php
-echo validation_errors('<div class="alert alert-warning">,</div>');
+echo validation_errors('<div class="alert alert-warning">','</div>');
 
 echo form_open_multipart(base_url('admin/daerah/edit/'.$daerah->id_daerah));
 
@@ -8,28 +8,35 @@ echo form_open_multipart(base_url('admin/daerah/edit/'.$daerah->id_daerah));
 <div class="col-md-6">
   <div class="form-group form-group-lg">
     <label>Nama Daerah</label>
-    <input type="text" name="nama_daerah" class"form-control" placeholder="Nama Daerah" value="<?php echo $daerah->nama_daerah ?>" required>
+    <input type="text" name="nama_daerah" class="form-control" placeholder="Nama Daerah" value="<?php echo $daerah->nama_daerah ?>" required>
   </div>
 </div>
 
 <div class="col-md-6">
   <div class="form-group form-group-lg">
     <label>Alamat</label>
-    <input type="text" name="alamat" class"form-control" placeholder="Masukkan Alamat" value="<?php echo $daerah->alamat ?>" required>
+    <input type="text" name="alamat" class="form-control" placeholder="Masukkan Alamat" value="<?php echo $daerah->alamat ?>" required>
   </div>
 </div>
 
 <div class="col-md-6">
   <div class="form-group form-group-lg">
     <label>Nomor Telepon</label>
-    <input type="text" name="telepon" class"form-control" placeholder="Nomor Telepon" value="<?php echo $daerah->telepon ?>"required>
+    <input type="text" name="telepon" class="form-control" placeholder="Nomor Telepon" value="<?php echo $daerah->telepon ?>"required>
   </div>
 </div>
 
 <div class="col-md-3">
   <div class="form-group form-group-lg">
     <label>Logo Daerah</label>
-    <input type="file" name="logo_daerah" class"form-control" required>
+    <input type="file" name="logo_daerah" class="form-control" required>
+  </div>
+</div>
+
+<div class="col-md-6">
+  <div class="form-group">
+    <label>Keterangan</label>
+    <input type="text" name="Keterangan" class="form-control" placeholder="Masukkan Keterangan" value="<?php echo $daerah->keterangan ?>"required>
   </div>
 </div>
 
