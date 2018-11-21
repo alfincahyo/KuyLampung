2a3dc4f00632474d2456a36f4258ce924fdbef24
Ferdio Naufal Haryadi <38089734+ferdionaufal@users.noreply.github.com>
2018-11-21 18:46:04 +0700

    Hotel Main Page

---
+++ application/views/hotel.php
@@ -0,0 +1,157 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hotel</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+	
+</head>
+
+<body>
+ 	<div class="container">
+
+    <h1 style="color: black; border-color: black; margin-left: -20px; margin-top: 20px;">Penginapan</h1>
+
+	<hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7" style="margin-top: 10px;">
+            <h2 class="featurette-heading">Hotel Novotel Bandar Lampung</h2>
+            <p class="lead">Novotel Lampung terletak di pusat kota Bandar Lampung, menghadap pantai di ujung selatan Pulau Sumatera. Hotel ini menawarkan 220 kamar modern, plus 2.400 m² ruang pertemuan dan acara, restoran, lounge bar, Lounge Premium, dan fasilitas kebugaran top seperti kolam renang luar ruang, kamar uap, jacuzzi, spa, dan gimnasium, serta Kapel Pernikahan. Terletak 10 menit dari kawasan bisnis dan dekat dengan daya tarik wisata, Novotel Lampung ideal untuk bisnis dan liburan.
+
+            Novotel Lampung adalah hotel bintang 4 yang memberi Anda akses mudah untuk mencapai dan menjelajahi tempat menarik terdekat di Bandar Lampung, seperti Museum Lampung, pusat perbelanjaan, banyak pantai indah (Sari Ringgung, Mutun, Ketapang). Terdapat pula Taman Bumi Kedaton, Taman Kupu-Kupu. Anda juga dapat mengunjungi lebih banyak destinasi wisata di Provinsi Lampung. Misalnya: Teluk Kiluan, Pantai Tanjung Setia, Taman Nasional Bukit Barisan, Pusat Konservasi Gajah Way Kambas.
+			</p>
+          </div>
+
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 250px;" src="<?=base_url()?>assets/img/hotel/Novotel.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2" style="margin-top: 10px;">
+            <h2 class="featurette-heading">TEMPOYAK</h2>
+            <p class="lead">Tempoyak merupakan sebuah makanan yang menggunakan bahan dasar buah durian yang sudah difermentasikan dan segera mengolahnya dengan campuran ikan. Ikan yang biasanya digunakan cukup beragam seperti ikan mujair, ikan teri, ikan mas dan lainnya. <br>
+            Bagi anda yang ingin membuat sambal maka harus mencampurkan bawang merah, bawang putih, cabe yang telah dihaluskan ataupun dipotong-potong kecil, kemudian tambahkan sedikit gula. Kemudian anda bisa mengorengnya hingga 3 menit, selanjutnya tempoyak bisa langsung dihidangkan dan dimakan.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/tempoyak.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7" style="margin-top: 10px;">
+            <h2 class="featurette-heading">GULAI TABOH</h2>
+            <p class="lead"> Gulai taboh memang dikenal sebagai makanan khas dari Lampung yang memiliki cita rasa yang sangat unik. Gulai taboh ini biasanyan terbuat dari ubi-ubian dan kacang-kacangan. Proses pembuatan gulai taboh terbilang sedikit mudah yakni dengan mencapurkan santan dengan kacang hijau ataupun kacang lainnya, serta juga bisa mencapurkan rebung dan kentang ke dalam masakan ini. Jika ingin membuat rasa yang berbeda maka anda bisa menambahkan daun sirih atau terasi ke dalamnya. Dengan adanya perpaduan antara ubi dengan terasi tersebut dapat membuat makanan terasa lebih lezat dilidah</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/gulaitaboh2.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2 " style="margin-top: 10px;">
+            <h2 class="featurette-heading">ENGKAK</h2>
+            <p class="lead">Engkak merupakan makanan yang memiliki rasa yang cukup manis. Bahan utama untuk pembuatan engkak ialah telur dan mentega yang dihaluskan menjadi tekstur yang sangat lembek. Engkak ini sangat cocok disantap sebagai makanan ringan dan juga sarapan. Bahan-bahan yang digunakan untuk pembuatan engkak ialah gula pasir, tepung ketan putih, telur ayam, susu kental manis, santan serta mentega.
+			Selanjutnya semua bahan tesebut bisa anda campurkan menjadi satu lalu aduk hingga merata. Setelahnya anda bisa memasukan adonan tersebut ke dalam loyang yang sudah dioleskan mentega dan juga di beri alas kertas berih. Anda bisa memanggang kue ini layaknya lapis segit.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/engkak2.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7" style="margin-top: 10px;">
+            <h2 class="featurette-heading">PINDANG </h2>
+            <p class="lead"> Pindang merupakan salah satu makanan khas dari kota Lampung yang wajib anda cicipi saat berkunjung ke kota yang penuh dengan pesona yang sangat menakjubkan ini. Pindang bisanya disajikan dengan kuah yang berwarna kuning. Pindang khas Lampung ini memiliki cita rasa yang sangat unik seperti asam gurih dengan aroma yang sangat kuat. Aroma yang kuat ini berasal dari duan kemangi yang dicampurkan ke dalam kuah. </p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/pindang2.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2" style="margin-top: 10px;">
+            <h2 class="featurette-heading">PANGGANG LAMPUNG</h2>
+            <p class="lead">Panggang merupakan makanan khas dari Lampung yang proses pembuatannya harus diasapi ataupun dipanggang. Sehingga tak heran jika makanan khas Lampung ini dikenal dengan nama panggang.
+			Panggang menggunakan ikan sebagai bahan utama dalam pembuatannya yang kemudian di panggang ataupun di asapi di atas bara api. Proses pemanganan ini juga tidak membutuhkan waktu yang lama, hanya saja anda harus menunggu hingga ikannya matang. Apabila ikan tersebut sudah matang maka anda bisa langsung memakannya.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/ikanpanggang.png" data-holder-rendered="true">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7" style="margin-top: 10px;">
+            <h2 class="featurette-heading">PISRO </h2>
+            <p class="lead"> Pisro merupakan jenis makanan dari olahan ikan yang sering disajikan sebagai jamuan makan sehari-hari masyarakat Pepadun. Dalam proses pembuatannya pisro menggunakan bahan utama ikan betok dan tembakang. Sementara sayur yang digunakan untuk pembuatan pisro mempunyai perpaduan rasa yang sama, manis, pedas serta gurih. Rasa gurih yang ada pada sayur ini berasal dari terasi ikan.Bahan lainnya yang dapat membuat rasa gurih pada sayur pisro yakni ikan bakar yang ada dalam kuah yang sudah dicampurkan dengan bumbu kental lainnya. Sedangkan rasa manis yang ada pada pisro ini berasal dari gula aren yang telah digiling bersamaan dengan bumbu seperti capai dan lainnya. </p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/pisro.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2" style="margin-top: 10px;">
+            <h2 class="featurette-heading">SAMBAL TEMPOYAK UDANG</h2>
+            <p class="lead"> Bagi anda yang sedang berada di Lampung jangan lupa untuk mencicipi sambal tempoyak udang ini. Karena dijamin anda tidak akan kecewa saat mencicipi sambal yang menawarkan rasa yang sangak maknyus ini. Pada dasarnya sambal tempoyak udang terbuat dari tomat dan cabai serta berbagai tambahan lainnya seperti pete, kunyit serta udang.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/sambaltempoyakudang.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7" style="margin-top: 10px;">
+            <h2 class="featurette-heading">SAMBAL ASAM KEMBANG </h2>
+            <p class="lead"> Sambal asam kembang merupakan salah satu makanan khas Lampung yang memiliki rasa cukup lezat dan nikmat. Jika mendengar namanya pasti anda mengira sambal ini terbuat dari bahan utamanya asam. Namun perlu diketahui, jika dalam proses pembuatan sambal asam kembang ini sama sekali tidak menggunakan asam, akan tetapi terbuat dari terasi yang dicampur dengan buah kemang seperti buah mangga muda.
+			Sambal asam kembang ini juga bisa disantap dengan makanan khas dari kota Lampung lainnya. Selain itu, anda juga bisa menjadikan sambal asam kembang ini sebagai oleh-oleh dari kota Lampung. </p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 380px;" src="<?=base_url()?>assets/img/culinary/sambalasamkembang.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+         <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2" style="margin-top: 10px;">
+            <h2 class="featurette-heading">KERIPIK NANGKA</h2>
+            <p class="lead"> Selain keripik pisang, ada juga makanan ringan khas Lampung lainnya yang bisa anda jadikan sebagai oleh-oleh yakni keripik nangka. Keripik nangka dengan keripik pisang memang tidak ada bedanya, karena sama-sama enak pada saat disantap. Bagi anda yang suka ngemil dan makanan ringan, patut mencoba keripik nangka khas Lampung ini. Karena pada saat anda mencoba keripik nangka ini dijamin anda akan selalu ketagihan dengan rasa yang dimilikinya. </p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 330px;" src="<?=base_url()?>assets/img/culinary/keripiknangka.jpg" data-holder-rendered="true">
+          </div>
+        </div>
+
+
+        </div>
+</body>
+</html>
