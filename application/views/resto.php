f67063a51ca2acead22b691b2adf57d68a1e14d4
Frisky Riana Kesumadewi <38089317+friskyriana@users.noreply.github.com>
2018-11-21 19:26:11 +0700

    Add files via upload

--- application/views/resto.php
+++ application/views/resto.php
@@ -84,7 +84,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="card-body">
         <h4 class="card-title" >Taman Santap Rumah Kayu</h4>
         <p class="card-text" > restoran yang menawarkan konsep asri dan hijau dimana terdapat taman dan saung lesehan apung yang dikelilingi kolam ikan.</p>
-        <a href=https://yussyakmal.com/ target="_top" class="btn btn-primary" >go website</a>
+        <a href=https://instagram.com/rumahkayulampung?utm_source=ig_profile_share&igshid=1rh3jlakns3lz target="_top" class="btn btn-primary" >go website</a>
         </div>
       </div>
       <br>
@@ -121,6 +121,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       </div>
     </div><br>
   </div>
+
+  <div class="row">
+    <div class="col-8 " >
+      <div class="judul">
+        <p><h3>Bakso Haji Soni</h3></p>
+      </div>
+      
+      <p> <br>
+      jika anda kebetulan berkunjung dan sedang berada di Lampung, cobalah untuk mencicipi Bakso Son Haji Sony. Di Lampung, khususnya di Bandar Lampung, bakso yang lebih dikenal dengan brand Bakso Sony termasuk bakso legendaris. Pasalnya hampir mayoritas warga Bandar Lampung jelas pernah merasakan kelezatan Bakso Sony. 
+      </p><br><br>
+      <div class="row">
+        <div class="col-sm-4" >
+          <img src="<?=base_url()?>assets/img/souveniers/money.png" alt="uang" height="50x" width="50px";>
+          <br><br>
+          15.000 - 25.000
+        </div>
+        <div class="col-sm-4" >
+          <img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";> 
+          <img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";> 
+          <img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";> 
+          <img src="<?=base_url()?>assets/img/souveniers/star.png" alt="bintang" height="25px" width="25px";> 
+          <img src="<?=base_url()?>assets/img/souveniers/halfstar.png" alt="bintang setengah" height="25px" width="25px"  margin-bottom = "20px";>  
+          <br><img src="<?=base_url()?>assets/img/souveniers/none.png"  height="50px" width="50px";>  
+          <br>
+          8.5 / 10
+        </div>
+        <div class="col-sm-4" >
+          <img src="<?=base_url()?>assets/img/souveniers/mytime.png" alt="waktu" height="50x" width="50px";>
+          <br><br>
+          10.00 - 22.00
+        </div>
+      </div>
+    </div>
+    <div class="col ">
+      <div class="card" >
+      <img src="<?=base_url()?>assets/img/souveniers/isi3.jpg" class="img-thumbnail" alt="Cinque Terre" width="100%" ;>
+        <div class="card-body">
+        <h4 class="card-title" >Bakso Haji Soni</h4>
+        <p class="card-text" >Bakso Sony begitu gurih bumbu tradisional, rasa bawang putih begitu terasa saat dikunyah</p>
+        <a href=https://instagram.com/bakso_sony_lampung_?utm_source=ig_profile_share&igshid=ep4atujwvale target="_top" class="btn btn-primary" >go website</a>
+        </div>
+      </div>
+      <br>
+    </div><br>
+  </div>
+
+  
 
 </body>
 </html>
