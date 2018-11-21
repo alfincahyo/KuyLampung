18d26fc60943b95d152de38566d01865953750e5
Ferdio Naufal Haryadi <38089734+ferdionaufal@users.noreply.github.com>
2018-11-21 16:37:37 +0700

    Add files via upload

--- application/views/lamsel/lamsel10.php
+++ application/views/lamsel/lamsel10.php
@@ -65,24 +65,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     Senin - Minggu <br> 06:00 – 00:00
                 </div>
             </div>
-            <br><br>
-            <div class="row" >
-                <div class="col-sm-6">
-                <img src="<?=base_url()?>assets/img/lamsel/siger/2.jpg"" class="img-thumbnail" alt="Cinque Terre" style="height: 227px; width: 400px;">   
-                </div>
-                <div>
-                <img src="<?=base_url()?>assets/img/lamsel/siger/3.jpg" class="img-thumbnail" alt="Cinque Terre" style="height: 227px; width: 400px;">
-                </div>
-            </div>
-            <br>
-            <div class="row" >
-                <div class="col-sm-6">
-                <img src="<?=base_url()?>assets/img/lamsel/siger/4.jpg" class="img-thumbnail" alt="Cinque Terre" style="height: 227px; width: 400px;">   
-                </div>
-                <div>
-                <img src="<?=base_url()?>assets/img/lamsel/siger/5.jpg" class="img-thumbnail" alt="Cinque Terre" style="height: 227px; width: 400px;">
-                </div>
-            </div>
             <?php
                 include "formkomentar.php";
             ?>
