385f4828ed04c5511213de4ee6e13ce711eadbf6
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-23 08:47:09 +0700

    Add files via upload

---
+++ application/views/lamsel/layout/head.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title><?php echo $artikel->judul ?></title>
+   <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="http://localhost/kuylampung/assets/css/komen.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="http://localhost/kuylampung/assets/css/home.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/kuylampung/assets/css/balam.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+</head>
---
+++ application/views/lamsel/layout/header.php
@@ -0,0 +1,37 @@
+
+	<html>
+<head>
+    <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link href="http://localhost/kuylampung/assets/css/header.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header style="background-image: url('http://localhost/kuylampung/assets/img/krakatau.jpg');s">
+
+    <ul>
+        <li><a href="http://localhost/kuylampung/home/beranda">Home</a></li>
+        <li><a href="http://localhost/kuylampung/home/location">Location</a></li>
+        <li><a href="http://localhost/kuylampung/home/culinar">Culinary</a></li>
+        <li><a href="http://localhost/kuylampung/home/souvenirs">Souvenirs</a></li>
+        <li style="float:right"><a href="http://localhost/kuylampung/Login">Login</a></li>
+        <li style="float:right"><a href="http://localhost/kuylampung/home/about_us">About Us</a></li>
+    </ul>
+
+    <div class="hero-text">
+    <h1>KUY LAMPUNG</h1>
+    <div class="link">
+    <a href="http://localhost/kuylampung/home/location" class="btn btn-one"> LOCATION </a>
+    <a href="http://localhost/kuylampung/home/culinar" class="btn btn-two"> CULINARY </a>
+    <a href="http://localhost/kuylampung/home/souvenirs" class="btn btn-three"> SOUVENIRS </a>
+    </div>
+    </div>
+    </header>
+
+
+
+</body>
+</html>
