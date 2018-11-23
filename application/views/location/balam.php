965a554247bec91208db6c7b6a80cb701b6a6d69
Adimas Sutanto <37900541+Adimas8@users.noreply.github.com>
2018-11-23 08:48:03 +0700

    Add files via upload

--- application/views/location/balam.php
+++ application/views/location/balam.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <title>
+<?php foreach ($daerah as $daerah) {?>
+    <?php echo $daerah->nama_daerah ?>
+  <?php } ?></title>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 
@@ -39,7 +44,7 @@ h1 {
     width: 25%;
 }
 
-/* Clear floats after rows */ 
+/* Clear floats after rows */
 .row:after {
     content: "";
     display: table;
@@ -83,144 +88,20 @@ h1 {
 
 <!-- Portfolio Gallery Grid -->
 <div class="row">
-  <div class="column">
-    <div class="content">
-      <img src="<?=base_url()?>assets/img/balam/mutun4.jpg" style="width:100%;">
-      <h3 style="margin-top: 10px;">Pantai Mutun</h3>
-      <p>Pantai Mutun dan Pulau Tangkil merupakan salah satu Tempat Wisata Menawan yang terletak di Provinsi Lampung.  
-            Lokasinya tak jauh dari Kota Bandar Lampung,hanya sekitar 10 Km dari pusat kota menuju ke arah kecamatan 
-            Padang Cermin Kabupaten Pesawaran.Pantai Mutun ini termasuk destinasi wisata favorit warga Bandarlampung dan sekitarnya.</p>
+  <?php foreach ($artikel as $artikel) { ?>
+  <div class="col-lg-4">
+      <img src="<?=base_url('assets/upload/gambar/'.$artikel->gambar)?>" class="img img-responsive" height="150px" width="310px">
+      <h3 style="margin-top: 10px;"><?php echo $artikel->judul ?></h3>
 
-      <a href="<?php echo site_url('balam/balam1'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/balam/puncakmas1.jpg" alt="Lights" style="width:100%;">
-      <h3 style="margin-top: 10px;">Puncak Mas</h3>
-      <p>Begitu masuk komplek wisata Puncak Mas, yang paling mencolok adalah adanya balon-balon raksasa dan rumah-rumah pohon. 
-            Balonnya dibuat seolah-olah hot air balloon gitu, tapi ga bisa terbang. Buat naik ke keranjang balon ada tangga, jadi 
-            balonnya semacam diparkir di menara kecil. Boleh foto di dalam keranjang balon itu, gratis.
-      </p>
-      <a href="<?php echo site_url('balam/balam2'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/balam/kupu3.png" alt="Nature" style="width:100%;">
-      <h3 style="margin-top: 10px;">Taman Kupu-Kupu</h3>
-      <p>
-      Tempat wisata dan sarana edukasi Taman Kupu-Kupu Gita Persada berada kecamatan Kemiling di kaki gunung Betung tepatnya di desa 
-            Suka manis , suasana yang sangat sejuk dan asri tentu sangat menarik minak wisatawan untuk berkunjung. Perjalanan tempuh dari 
-            kota Bandar Lampung sekitar 30 menit.
-      </p>
-      <a href="<?php echo site_url('balam/balam3'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/balam/museum2.jpg" alt="Mountains" style="width:100%;">
-      <h3 style="margin-top: 10px;">Museum Lampung</h3>
-      <p> Museum Lampung mulai dibangun tahun 1975, peletakan batu pertama tahun 1978. Diresmikan oleh Menteri Pendidikan dan Kebudayaan 
-            kala itu, Prof. Dr. Fuad Hasan, pada 24 September 1988. Bertepatan dengan peringatan Hari Aksara Internasional yang dipusatkan 
-            di PKOR Way Halim.
-</p>
-      <a href="<?php echo site_url('balam/balam4'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-<!-- END GRID -->
-</div>
+      <?php echo character_limiter($artikel->isi,200); ?>
 
-<div class="content">
-  <img src="<?=base_url()?>assets/img/balam/horti1.jpg" alt="Bear" style="width:100%;">
-  <h3 style="margin-top: 10px;">Horti Park</h3>
-  <p>Lingkungan yang hijau dan asri juga dapat tercipta melalui kegiatan tanam yang bukan hanya dilakukan oleh lingkungan pertanian tetapi juga pemanfaatan lahan semaksimal mungkin. Adalah hortikultura yang merupakan salah satu metode budidaya pertanian modern yang saat ini sedang marak diterapkan. Bidang kerja dari hortikultura meliputi pembenihan, pembibitan, kultur jaringan, produksi tanaman, hama dan penyakit, panen, pengemasan dan distribusi.</p>
-    <a href="<?php echo site_url('balam/balam5'); ?>">
+      <a href="<?php echo base_url('balam/read/'.$artikel->id_artikel) ?>">
                 Baca Selengkapnya >>>>>
                 </a>
-</div>
-
-<div class="row">
-  <div class="column">
-    <div class="content">
-      <img src="<?=base_url()?>assets/img/balam/bukit2.jpeg" style="width:100%;">
-      <h3 style="margin-top: 10px;">Bukit Sakura</h3>
-      <p>Bukit Sakura Kemiling sangat pas sebagai tempat berlibur bagi kaum muda mudi, sebab di sana tersedia banyak spot foto kekinian yang menjadi spot favorit bagi para pengunjung untuk berselfie. Buat liburan keluarga juga bagus, di sana bisa bersantai dan jalan-jalan menikmati indahnya panorama alam yang luar biasa.</p>
-
-      <a href="<?php echo site_url('balam/balam6'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
   </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/balam/lembah1.jpg" alt="Lights" style="width:100%;">
-      <h3 style="margin-top: 10px;">Lembah Hijau</h3>
-      <p>
-        Taman Wisata Lembah
-Hijau adalah taman wisata keluarga berorentasi lingkungan, memadukan antara 
-            rekreasi, nuansa kesegaran alam nan hijau dan pengetahuan, taman wisata yang 
-            berlokasi di  Bandar Lampung yang menjadi ibukota Provinsi Lampung ini 
-            berdiri diatas kawasan lembah dengan luas 30 Ha di Jalan Raden Imba Kesuma Ratu
-      </p>
-      <a href="<?php echo site_url('balam/balam7'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/balam/wawai2.jpg" alt="Nature" style="width:100%;">
-      <h3 style="margin-top: 10px;">Alam Wawai</h3>
-      <p>
-      Mencari taman yang indah dan asri khusus wisata Lampung dengan konsep unik sulit ditemukan.
-Namun Taman Wisata Alam Wawai Eco Park menawarkan tempat yang berbeda dan bisa menjadi destinasi di akhir pekan.Menuju ke lokasi tidaklah sulit dari pusat kota Bandar Lampung dengan hanya berjarak sekitar 1 km. Jarak tempuh setengah jam<br>telah sampai ke lokasi.
-      </p>
-      <a href="<?php echo site_url('balam/balam8'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/balam/pinus5.jpg" alt="Mountains" style="width:100%;">
-      <h3 style="margin-top: 10px;">Hutan Pinus</h3>
-      <p> Zona 235 adalah tempat atau kawasan hutan yang terdapat villa dan digunakan sebagai tempat outbond, training centre, paintball, 
-            dan camping. Letaknya masih disekitar kawasan wisata Sukadanaham Bandar Lampung. Untuk mencapai ke Zona 235 cukup ditempuh dengan
-             15 menit perjalanan dari Tanjung Karang menggunakan kendaraan.
-</p>
-      <a href="<?php echo site_url('balam/balam9'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-<!-- END GRID -->
-</div>
-
-<div class="content">
-  <img src="<?=base_url()?>assets/img/balam/rusa1.jpg" alt="Bear" style="width:100%;">
-  <h3 style="margin-top: 10px;">Penangkaran Rusa</h3>
-  <p>
-    Penangkaran Rusa Taman Hutan Raya Wan Abdul Rachman atau Penangkaran Rusa Tahura WAR
-merupakan sebuah tempat penangkaran rusa yang secara teknis dikelola oleh polisi hutan. Penangkaran Rusa Tahura War secara resmi 
-            mendapat izin pada tanggal 7 Desember 2012 dengan luas lahan sekitar 2 hektar.
-Rusa yang dikembangkan di penangkaran ini adalah Rusa Timur.
-  </p>
-    <a href="<?php echo site_url('balam/balam10'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-</div>
-
+  <?php } ?>
 <!-- END MAIN -->
 </div>
-
+</div>
 </body>
 </html>
--- application/views/location/lamsel.php
+++ application/views/location/lamsel.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <title>
+<?php foreach ($daerah as $daerah) {?>
+    <?php echo $daerah->nama_daerah ?>
+  <?php } ?></title>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 @import url('https://fonts.googleapis.com/css?family=Expletus+Sans');
@@ -42,7 +47,7 @@ h1 {
     width: 25%;
 }
 
-/* Clear floats after rows */ 
+/* Clear floats after rows */
 .row:after {
     content: "";
     display: table;
@@ -86,130 +91,20 @@ h1 {
 
 <!-- Portfolio Gallery Grid -->
 <div class="row">
-  <div class="column">
-    <div class="content">
-      <img src="<?=base_url()?>assets/img/lamsel/pasirputih.jpg" style="width:100%; height: 23%;">
-      <h3 style="margin-top: 10px;">Pantai Pasir Putih</h3>
-      <p>Salah satu obyek wisata yang terkenal di Lampung adalah Pantai Pasir Putih yang terletak di Jalan Trans Sumatera.
-      <a href="<?php echo site_url('lamsel/lamsel1'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/lamsel/embe.jpg" alt="Lights" style="width:100%; height: 23%;">
-      <h3 style="margin-top: 10px;">Pantai Embe (Embe Beach)</h3>
-      <p>Pantai Embe Kalianda. Siapa sih yang gak suka bermain di Pantai Pasir Putih yang lembut nan indah? Apalagi ditemani cahaya matahari sore yang keemasan.
-   
-      </p>
-      <a href="<?php echo site_url('lamsel/lamsel2'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/lamsel/batugranit.jpg" alt="Nature" style="width:100%; height: 23%;">
-      <h3 style="margin-top: 10px;">Taman Batu Granit</h3>
-      <p>
-      Sebagai salah satu provinsi dengan garis pantai terpanjang sumatera, Provinsi Lampung juga mengandung beragam potensi geologi yang juga menjanjikan
-      </p>
-      <a href="<?php echo site_url('lamsel/lamsel3'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/lamsel/Sebesi.jpg" alt="Mountains" style="width:100%; height: 23%;">
-      <h3 style="margin-top: 10px;">Pulau Sebesi</h3>
-      <p>Pulau Sebesi menjadi salah satu pulau yang sedang dikembangkan menjadi daerah tujuan wisata andalan Lampung Selatan selain Pulau Krakatau.
-      </p>
-      <a href="<?php echo site_url('lamsel/lamsel4'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-<!-- END GRID -->
-</div>
+  <?php foreach ($artikel as $artikel) { ?>
+  <div class="col-lg-4">
+      <img src="<?=base_url('assets/upload/gambar/'.$artikel->gambar)?>" class="img img-responsive" height="150px" width="310px">
+      <h3 style="margin-top: 10px;"><?php echo $artikel->judul ?></h3>
 
-<div class="content">
-  <img src="<?=base_url()?>assets/img/lamsel/karet.jpg" alt="Bear" style="width:100%">
-  <h3 style="margin-top: 10px;">Kebun Karet Trikora PTPN 7</h3>
-  <p>Jika kamu sedang pergi ke Lampung dan ditanya tempat mana yang sedang ngetrend untuk pengambilan foto? Jawabannya pastilah Kebun Karet Trikora. 
-  </p>
-    <a href="<?php echo site_url('lamsel/lamsel5'); ?>">
+      <?php echo character_limiter($artikel->isi,200); ?>
+
+      <a href="<?php echo base_url('lamsel/read1/'.$artikel->id_artikel) ?>">
                 Baca Selengkapnya >>>>>
                 </a>
-</div>
-
-
-<div class="row">
-  <div class="column">
-    <div class="content">
-      <img src="<?=base_url()?>assets/img/lamsel/mengkudu.jpg" style="width:100%; height: 23%;">
-      <h3 style="margin-top: 10px;">Pulau Mengkudu</h3>
-      <p>Pulau Mengkudu merupakan objek wisata kekinian yang belakangan naik daun melalui media sosial. sebuah tempat indah yang kerap mendapat julukan sebagai Tanah Lot-nya Lampung.
-      </p>
-
-      <a href="<?php echo site_url('lamsel/lamsel6'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
   </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/lamsel/guci.jpg" alt="Lights" style="width:100%; height: 23%;">
-      <h3 style="margin-top: 10px;">Pantai Guci Batu Kapal</h3>
-      <p>Pantai Guci Batu Kapal menjadi salah satu hal penting yang perlu anda kunjungi,masih ada banyak hal lainnya yang tentunya akan membuat liburan anda semakin menyenangkan.
-      
-      </p>
-      <a href="<?php echo site_url('lamsel/lamsel7'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
+  <?php } ?>
   </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/lamsel/sembalang.jpg" alt="Nature" style="width:100%; height: 23%;">
-      <h3 style="margin-top: 10px;">Pantai Sebalang</h3>
-      <p>
-      Pantai adalah salah satu destinasi yang bagus untuk berlibur dan refreshing. Pemandangannya bagus disertai dengan pasir hitam maupun pasir putih yang turut menghiasi tepi pantai.      
 
-      </p>
-      <a href="<?php echo site_url('lamsel/lamsel8'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-  <div class="column">
-    <div class="content">
-    <img src="<?=base_url()?>assets/img/lamsel/Marina.jpg" alt="Mountains" style="width:100%; height: 23%;">
-      <h3 style="margin-top: 10px;">Pantai Marina</h3>
-      <p>Lokasinya tak jauh dari kota, jadi jangan ngaku anak travelling ya kalo belum tahu pantai ini, karena pantai ini tersembunyi lokasinya Selain karena lokasinya yang tersembunyi dan terpencil.
-  
-</p>
-      <a href="<?php echo site_url('lamsel/lamsel9'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-    </div>
-  </div>
-<!-- END GRID -->
-</div>
-
-<div class="content">
-  <img src="<?=base_url()?>assets/img/lamsel/Siger.jpeg" alt="Bear" style="width:100%">
-  <h3 style="margin-top: 10px;">Menara Siger</h3>
-  <p>Obyek Wisata Menara Siger di Lampung Selatan Lampung adalah salah satu tempat wisata yang berada di Bukit Gamping, Kecamatan Bakauheni, Kabupaten Lampung Selatan, Indonesia.
-
-</p>
-    <a href="<?php echo site_url('lamsel/lamsel10'); ?>">
-                Baca Selengkapnya >>>>>
-                </a>
-</div>
-
-<!-- END MAIN -->
 </div>
 
 </body>
